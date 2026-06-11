1eaecbce94da9ee852d51bff52a27e6690e67e25
jeanzuck <jeanzuck.dev@gmail.com>
2026-06-11 14:25:02 +0700

    chore(examples): remove boolean example

--- examples/boolean.rs
+++
@@ -1,7 +0,0 @@
-fn main() {
-    let enabled = obfany::obfbool!(true);
-    let disabled = obfany::obfbool!(false);
-
-    println!("enabled: {}", enabled);
-    println!("disabled: {}", disabled);
-}
